c2d47db1f338903b9d650ba6d1b4ae638ec5719b
Karthik M <karthikm7483@gmail.com>
2023-07-13 12:13:28 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,20 +3,20 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
 
-    DeluxePizza dp = new DeluxePizza(true);
-    dp.addExtraCheese();
-    dp.addExtraToppings();
-    dp.addExtraToppings();
-    dp.addTakeaway();
-//    System.out.println(dp.getBill());
-
-    Pizza p = new Pizza(false);
-    p.addExtraToppings();
-    p.addTakeaway();
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addExtraCheese();
-//    System.out.println(p.getBill());
+//    DeluxePizza dp = new DeluxePizza(true);
+//    dp.addExtraCheese();
+//    dp.addExtraToppings();
+//    dp.addExtraToppings();
+//    dp.addTakeaway();
+////    System.out.println(dp.getBill());
+//
+//    Pizza p = new Pizza(false);
+//    p.addExtraToppings();
+//    p.addTakeaway();
+//    p.addExtraCheese();
+//    p.addExtraToppings();
+//    p.addExtraCheese();
+////    System.out.println(p.getBill());
 
   }
 }
